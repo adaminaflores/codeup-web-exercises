7fc138d272c84720f1b5a64c87dd07c92a800558
Adamina Flores <adaminaflores19@gmail.com>
2021-08-12 15:38:21 -0500

    feat: new pop ups for Exercise 1

---
+++ js/external.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello from External Javascript");
+
+alert("Welcome to my Website");
+
+var userInput = prompt("Hey, what is your favorite color?");
+alert('My favorite color is also:'+ userInput);
