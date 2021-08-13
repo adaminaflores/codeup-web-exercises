de69582919cea75ac64112e7819a002d39fd21e0
Adamina Flores <adaminaflores19@gmail.com>
2021-08-13 15:31:03 -0500

    Exercises from Thursday August 12th

--- js/external.js
+++ js/external.js
@@ -10,12 +10,26 @@
 var movieRental = prompt("How many days would you like to rent this movie?");
 alert("For this many days it will cost" + movieRental*3);
 
-
+//How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
 var workWeek = prompt("How many hours did you work this week?");
-var gRate = 400;
-var aRate = 380;
-var fRate = 350;
+var gRate = parseInt(400 * 6);
+var aRate = parseInt(380 * 4);
+var fRate = parseInt(350 * 10);
+var total = (gRate + aRate + fRate);
+alert("This is how much you will receive this week:" + total);
+
+//A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule
+
+var notFull = confirm("Is the class you want to attend full?");
+var classSchedule = confirm("Does this class conflict with your current schedule?");
+alert("You can join this class a " + !notFull && !classSchedule + "statement");
 
 
+
+//A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products
+var productOffer = confirm("Is this offer still valid?");
+var nonMember = parseInt(prompt("How many items do you have?"));
+var premiumMember = parseInt(prompt("Are you a member?"));
+alert("This offer is valid :" + !nonMember && !premiumMember);
 
 
