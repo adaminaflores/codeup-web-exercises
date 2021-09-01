b78efa2f132244cf1147f06dd4beb5372e251725
Adamina Flores <adaminaflores19@gmail.com>
2021-09-01 15:10:38 -0500

    While loops exercise

---
+++ js/while.js
@@ -0,0 +1,19 @@
+"use strict";
+// console.log("Hii");
+
+// While Loop Exercise:
+var i = 2;
+while(i < 100000) {
+    console.log(i);
+    i *= 2;
+}
+
+// Do While Loop Exercise:
+//60 cones
+// random num between 1-5 is 4
+//"5 cones sold" if there are enough cones
+//"Cannot sell you 6 cones I only have 3" if there are not enough
+//"Yay! I sold them all!" if there are no more cones
+var cones = 60;
+var customers = 4;
+
