63202a9706e0e3ae474163af8f62ccb87363d903
Adamina Flores <adaminaflores19@gmail.com>
2021-09-09 09:23:10 -0500

    Exercises that were pushed on 9/9

---
+++ js/warmup2.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+
+
+function tellFortune(w,x,y,z) {
+
+    var output = prompt("You will be a " + w + " in " + x + " and married to " + y + " with " + z + " kids")
+
+    return output;
+}
---
+++ js/warmup3.js
@@ -0,0 +1,6 @@
+"use strict";
+
+function calculateDogAge(x){
+    var x = x * 7
+    return alert("Your dog is this age in doggy years: " + x) ;
+}
