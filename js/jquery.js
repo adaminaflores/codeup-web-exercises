6c0da918b47734118f0b67a8480860579463d2aa
Adamina Flores <adaminaflores19@gmail.com>
2021-09-28 13:36:36 -0500

    Selector part of the jQuery exercises finished

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,8 @@
 "use strict";
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+$('.codeup').css('border', '1px solid red');
+$('li').css('font-size', '20px');
+$('h1 , li, p').css('background-color','yellow');
