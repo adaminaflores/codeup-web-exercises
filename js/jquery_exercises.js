a91540b299881d617d1a437714bba06e8bf10159
Adamina Flores <adaminaflores19@gmail.com>
2021-09-28 14:28:16 -0500

    jQuery Mouse Events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,8 +1,25 @@
 "use strict";
 
-// $(document).ready(function() {
-//     alert( 'The DOM has finished loading!' );
+// $('h1').ready(function() {
+//     alert( 'Hello There!' );
 // });
-$('.codeup').css('border', '1px solid red');
-$('li').css('font-size', '20px');
-$('h1 , li, p').css('background-color','yellow');
+// $('.codeup').css('border', '1px solid red');
+// $('li').css('font-size', '20px');
+// $('h1 , li, p').css('background-color','yellow');
+
+//.click() function using jQuery
+$('#main-heading').click(function(){
+    $(this).css('background-color', 'blue');
+    });
+//.dblclick() function using jQuery
+$('p').dblclick(function(){
+    $(this).css('font-size', '18px')
+});
+//.hover(handlerIn, handlerOut) function using jQuery
+$('li').hover(function(){
+    $(this).css('color','red');
+    },
+    function(){
+    $(this).css('color', 'black');
+    }
+);
