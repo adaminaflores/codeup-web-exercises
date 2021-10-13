66be7cd56e95a7c52ec45beb57fc6f1bd444b8bf
Adamina Flores <adaminaflores19@gmail.com>
2021-10-13 12:28:51 -0500

    ES6 exercises and lecture

--- js/es6.js
+++ js/es6.js
@@ -27,11 +27,11 @@ const users = [
     email: 'justin@codeup.com',
     languages: ['html', 'css', 'javascript', 'php']
   },
-  {
-    name: 'adamina',
-    email: 'adaminaflores19@gmail.com',
-    languages: ['html', 'css', 'javascript']
-  }
+  // {
+  //   name: 'adamina',
+  //   email: 'adaminaflores19@gmail.com',
+  //   languages: ['html', 'css', 'javascript']
+  // }
 
 ];
 
@@ -44,18 +44,19 @@ const email = 'adaminaflores19@gmail.com';
 const languages = ['html', 'css', 'javascript'];
 
 // TODO: rewrite the object literal using object property shorthand
-users.push({
-  name: name,
-  email: email,
-  languages: languages
-});
+// users.push({
+//   name: name,
+//   email: email,
+//   languages: languages
+// });
+
 <!--Re written-->
- users.push = (
+ users.push({
   name,
   email,
   languages
 
- );
+});
 
 
 // TODO: replace `var` with `let` in the following variable declarations
@@ -63,36 +64,42 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-  return emails.push(user.email);
-});
-users.forEach(function(user) {
-  return names.push(user.name);
-});
+// users.forEach(function(user) {
+//   return emails.push(user.email);
+// });
+// users.forEach(function(user) {
+//   return names.push(user.name);
+// });
 <!--Re written-->
- users.forEach = (user) => email.push(user.email);
+ users.forEach = (user) => emails.push(user.email);
  users.forEach = (user) => names.push(user.name);
 
 
 // TODO: replace `var` with `let` in the following declaration
+
+
 let developers = [];
-users.forEach(function(user) {
+users.forEach(({name ,email, languages}) => developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(', ')}`));
+
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
-
-
+  // let user = {
+  //   name: user.name,
+  //   email: user.email,
+  //   languages: user.languages
+  //
+  //
+  // };
+  // const {name, email, languages} = user;
 
 
   // TODO: rewrite the assignment below to use template strings
 
 
 
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
-});
+
+
 
 // TODO: Use `let` for the following variable
 let list = '<ul>';
